04f18f0d45e5e9ce22df67cf867b670dc69f496b
skaltman <skaltman@stanford.edu>
2019-09-06 14:22:23 -0700

    initial commit

--- _common.R
+++ _common.R
@@ -17,21 +17,4 @@ knitr::opts_chunk$set(
 
 image_dpi <- 125
 
-# Stamps plots with a tag 
-# Idea from Claus Wilke's "Data Visualization" https://serialmentor.com/dataviz/
-stamp <- function(
-  tag = "Bad", tag_color = "#B33A3A", tag_size = 16, tag_padding = 1
-)
-{
-  list(
-    theme(
-      plot.tag = element_text(color = tag_color, size = tag_size),
-      plot.tag.position = "topright"
-    ),
-    labs(
-      tag =
-        str_pad(tag, width = str_length(tag) + tag_padding, side = "left")
-    )
-  )
-}
 
