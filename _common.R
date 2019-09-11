d03919668ee24470344dd41a8da1c168fb34c3aa
skaltman <skaltman@stanford.edu>
2019-09-11 16:11:34 -0700

    update

--- _common.R
+++ _common.R
@@ -18,4 +18,8 @@ knitr::opts_chunk$set(
 
 image_dpi <- 125
 
+label_model_line <- function(error_metric, a_0, a_1) {
+  str_glue("{error_metric}\na_0 = {round(a_0, 2)}\na_1 = {round(a_1, 2)}")
+}
+
 
