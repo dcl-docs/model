f855febb245c6360755f59294ac9597fe04eaee5
skaltman <skaltman@stanford.edu>
2020-02-28 14:30:13 -0800

    update; add fit chapter

--- _common.R
+++ _common.R
@@ -7,7 +7,7 @@ options(
 )
 
 knitr::opts_chunk$set(
-  collapse = TRUE,
+  # collapse = TRUE,
   comment = "#>",
   fig.align = 'center',
   fig.asp = 0.618,  # 1 / phi
@@ -16,6 +16,24 @@ knitr::opts_chunk$set(
 
 image_dpi <- 125
 
+label_model_line <- function(error_metric, a_0, a_1) {
+  str_glue("{error_metric}\na_0 = {round(a_0, 2)}\na_1 = {round(a_1, 2)}")
+}
+
+annotate_model <- function(x, y, error_metric = "RMSE", model) {
+  annotate(
+    geom = "label",
+    x = x,
+    y = y,
+    label = 
+      label_model_line(
+        error_metric = error_metric, 
+        a_0 = coef(model)[[1]], 
+        a_1 = coef(model)[[2]]
+      ),
+    hjust = 0
+  ) 
+}
 
 
 
