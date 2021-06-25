d3081ce823b14009672a072bc600da6a3c835e39
Bill Behrman <behrman@stanford.edu>
2021-06-25 15:15:04 -0700

    Remove superseded code

--- _common.R
+++ _common.R
@@ -15,25 +15,3 @@ knitr::opts_chunk$set(
 )
 
 image_dpi <- 125
-
-label_model_line <- function(error_metric, a_0, a_1) {
-  str_glue("{error_metric}\na_0 = {round(a_0, 2)}\na_1 = {round(a_1, 2)}")
-}
-
-annotate_model <- function(x, y, error_metric = "RMSE", model) {
-  annotate(
-    geom = "label",
-    x = x,
-    y = y,
-    label = 
-      label_model_line(
-        error_metric = error_metric, 
-        a_0 = coef(model)[[1]], 
-        a_1 = coef(model)[[2]]
-      ),
-    hjust = 0
-  ) 
-}
-
-
-
